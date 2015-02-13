901d0027725b01eb49b8eade0d3efcd38f6a6505
hhlarsen <hh.larsen@stud.uis.no>
2015-02-13 09:02:52 +0100

    showing previous notes almost :space_invader:

--- PitchTryTest/app/src/main/java/com/rec/ceciliemarie/pitchtrytest/MainActivity.java
+++ PitchTryTest/app/src/main/java/com/rec/ceciliemarie/pitchtrytest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rec.ceciliemarie.pitchtrytest;
 
 import java.lang.Thread;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.rec.ceciliemarie.pitchtrytest.NoteArray;
@@ -18,6 +19,7 @@ public class MainActivity extends ActionBarActivity {
 
     Thread pitch_detector_thread_;
     public String written = " ";
+    public ArrayList<String> noteArray = new ArrayList();
 
     /** Called when the activity is first created. */
     @Override
@@ -47,6 +49,13 @@ public class MainActivity extends ActionBarActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
+    @Override
+    public String toString() {
+        return "MainActivity{" +
+                "noteArray=" + noteArray +
+                '}';
+    }
+
     public void ShowPitchDetectionResult( final double pitch) {
 
         final TextView changeFreq = (TextView) findViewById(R.id.freqTextview);
@@ -61,16 +70,30 @@ public class MainActivity extends ActionBarActivity {
 
         System.out.println(nearestNote + "  nearest note :)");
 
-
         changeFreq.setText(nearestNote);
+
 
         String earlierNotes = (String) setPrevNotes.getText();
 
         if(!nearestNote.equals(written)){
 
-            setPrevNotes.setText(earlierNotes + " " + nearestNote);
+            if(noteArray.size() == 14){
+                noteArray.remove(0);
+                noteArray.trimToSize();
+            }
+
+            noteArray.add(nearestNote);
+            setPrevNotes.setText(noteArray.toString());
+
+            /*
+            noteArray.toString()
+
+            setPrevNotes.setText(earlierNotes + " " + nearestNote);*/
             written = nearestNote;
         }
+
+
+
     }
 
 
