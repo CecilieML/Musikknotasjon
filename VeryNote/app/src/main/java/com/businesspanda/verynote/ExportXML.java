91b59009d55a68af4ad7e75f114aab57d74d3e75
CecilieML <cm.langfeldt@stud.uis.no>
2015-03-03 14:40:16 +0100

    it's gonna blow! :fireworks:

---
+++ VeryNote/app/src/main/java/com/businesspanda/verynote/ExportXML.java
@@ -0,0 +1,44 @@
+package com.businesspanda.verynote;
+
+import android.content.Context;
+
+import org.jfugue.MusicStringParser;
+import org.jfugue.MusicXmlRenderer;
+import org.jfugue.Pattern;
+import org.jfugue.*;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+
+import nu.xom.Serializer;
+
+
+/**
+ * Created by CecilieMarie on 03.03.2015.
+ */
+public class ExportXML implements Serializable {
+
+    void export(Context context) {
+        try {
+            FileOutputStream file = context.openFileOutput("music.xml", Context.MODE_PRIVATE);
+
+            MusicXmlRenderer renderer = new MusicXmlRenderer();
+            MusicStringParser parser = new MusicStringParser();
+            parser.addParserListener(renderer);
+
+            Pattern pattern = new Pattern("C D E F G A B |");
+            parser.parse(pattern);
+
+            Serializer serializer = new Serializer(file, "UTF-8");
+            serializer.setIndent(4);
+            serializer.write(renderer.getMusicXMLDoc());
+
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
