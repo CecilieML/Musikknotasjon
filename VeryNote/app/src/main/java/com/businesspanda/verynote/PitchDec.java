5c65dc2819765a67411d30aaa9e33942c1d75363
CecilieML <cm.langfeldt@stud.uis.no>
2015-03-17 17:43:27 +0100

    numberchange :1234:

--- VeryNote/app/src/main/java/com/businesspanda/verynote/PitchDec.java
+++ VeryNote/app/src/main/java/com/businesspanda/verynote/PitchDec.java
@@ -45,11 +45,11 @@ import com.businesspanda.verynote.MainActivity;
 public class PitchDec implements Runnable {
     private static String LOG_TAG = "PitchDetector";
 
-    private final static int RATE = 8000; //was 44100
+    private final static int RATE = 16000; //was 44100, then 8000 when it worked
     private final static int CHANNEL_MODE = AudioFormat.CHANNEL_IN_MONO;
     private final static int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 
-    int bufferSize = 4096 ;//AudioRecord.getMinBufferSize(RATE, CHANNEL_MODE, ENCODING); //=4096
+    int bufferSize = 8192 ;//AudioRecord.getMinBufferSize(RATE, CHANNEL_MODE, ENCODING); //=4096
     int SOURCE = MediaRecorder.AudioSource.MIC;
 
 
@@ -60,7 +60,7 @@ public class PitchDec implements Runnable {
 
 
     //private final static int BUFFER_SIZE_IN_MS = 3000;
-    private final static int CHUNK_SIZE_IN_SAMPLES = 1024; // = 2 ^ //////was 1024/////
+    private final static int CHUNK_SIZE_IN_SAMPLES = 8192; // = 2 ^ //////was 1024/////
     // CHUNK_SIZE_IN_SAMPLES_POW2
     private final static int CHUNK_SIZE_IN_MS = 1000 * CHUNK_SIZE_IN_SAMPLES
             / RATE;
